1a0563b1ef769860a3631ab50586d49184a5b37d
Igor.Szafalowicz <igor.szafalowicz@o2.pl>
2022-05-09 20:43:30 +0200

    Initial commit

---
+++ src/main/java/pl/carsrental/CarsrentalApplication.java
@@ -0,0 +1,13 @@
+package pl.carsrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsrentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsrentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/carsrental/CarsrentalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.carsrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsrentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
